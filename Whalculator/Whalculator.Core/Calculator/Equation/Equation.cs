60ce6ffe3ff8141d17ef16d0f0422eaf44ce4acd
Ryan-Afzal <ryan.afzal@outlook.com>
2019-09-15 20:25:37 -0700

    Added more expression stuff

--- Whalculator/Whalculator.Core/Calculator/Equation/Equation.cs
+++ Whalculator/Whalculator.Core/Calculator/Equation/Equation.cs
@@ -1,9 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Whalculator.Core.Calculator.Equation.Expression;
 
 namespace Whalculator.Core.Calculator.Equation {
-    public class Equation {
+    public class Equation : ICloneable {
 
+        private readonly IExpression leftExpression;
+        private readonly IExpression rightExpression;
+
+        public Equation(IExpression left, IExpression right) {
+            this.leftExpression = left;
+            this.rightExpression = right;
+        }
+
+        public ICloneable Clone() {
+            return new Equation(
+                (IExpression)this.leftExpression.Clone(),
+                (IExpression)this.rightExpression.Clone());
+        }
     }
 }
---
+++ Whalculator/Whalculator.Core/Calculator/Equation/Expression/IExpression.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Whalculator.Core.Calculator.Equation.Expression {
+    public interface IExpression : ICloneable {
+        IExpression GetExactValue(ExpressionEvaluationArgs args);
+        double GetValue(ExpressionEvaluationArgs args);
+    }
+}
