95737f0c1acb0a7dd9557dd3ebf245f7254e49b2
Ryan-Afzal <ryan.afzal@outlook.com>
2019-09-16 08:36:12 -0700

    Added more stuff

---
+++ Whalculator/Whalculator.Core/Calculator/Equation/Expression/ISolvable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Whalculator.Core.Calculator.Equation.Expression {
+    public interface ISolvable : ICloneable {
+        ISolvable GetExactValue(ExpressionEvaluationArgs args);
+        double GetValue(ExpressionEvaluationArgs args);
+    }
+}
---
+++ Whalculator/Whalculator.Core/Calculator/Equation/Expression/ITerm.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Whalculator.Core.Calculator.Equation.Expression {
+    public interface ITerm : ISolvable {
+        
+    }
+}
