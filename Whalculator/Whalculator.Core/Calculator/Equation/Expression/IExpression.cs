f2f8ec8418bd5acc9da820c1eb67f66ffb621460
Ryan-Afzal <ryan.afzal@outlook.com>
2019-09-20 07:33:59 -0700

    Update IExpression.cs

--- Whalculator/Whalculator.Core/Calculator/Equation/Expression/IExpression.cs
+++ Whalculator/Whalculator.Core/Calculator/Equation/Expression/IExpression.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Whalculator.Core.Calculator.Equation.Expression {
-    public interface IExpression : ICloneable {
-        IExpression GetExactValue(ExpressionEvaluationArgs args);
-        double GetValue(ExpressionEvaluationArgs args);
+    public interface IExpression : ISolvable {
+
     }
 }
