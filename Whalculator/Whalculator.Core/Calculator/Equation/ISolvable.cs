f4f99d3b8d4be8a04717611c167da417d40767c3
Ryan-Afzal <ryan.afzal@outlook.com>
2019-09-23 14:55:51 -0700

    Worked more on implementing the equation system

--- Whalculator/Whalculator.Core/Calculator/Equation/ISolvable.cs
+++ Whalculator/Whalculator.Core/Calculator/Equation/ISolvable.cs
@@ -5,8 +5,24 @@ using System.Text;
 namespace Whalculator.Core.Calculator.Equation {
 
 	public interface ISolvable {
+		/// <summary>
+		/// Gets an exact value answer, expressed as an ISolvable
+		/// </summary>
+		/// <param name="args"></param>
+		/// <returns></returns>
 		ISolvable GetExactValue(ExpressionEvaluationArgs args);
+
+		/// <summary>
+		/// Gets the decimal value of the answer. Usually this is getting the double value of the exact answer.
+		/// </summary>
+		/// <param name="args"></param>
+		/// <returns></returns>
 		double GetDoubleValue(ExpressionEvaluationArgs args);
+
+		/// <summary>
+		/// Gets an ISolvable basically equal to this one. 
+		/// </summary>
+		/// <returns>Returns a functional copy of this ISolvable</returns>
 		ISolvable Clone();
 	}
 }
--- Whalculator/Whalculator.Core/Calculator/Equation/Term.cs
+++ Whalculator/Whalculator.Core/Calculator/Equation/Term.cs
@@ -3,9 +3,31 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Whalculator.Core.Calculator.Equation {
-	public class Term : ISolvable {
+	public sealed class Term : ISolvable {
 
 		private readonly ISolvable[] coefficients;
 
+		public Term(params ISolvable[] coefficients) {
+			this.coefficients = coefficients;
+		}
+
+
+
+		public ISolvable Clone() {
+			var newArray = new ISolvable[this.coefficients.Length];
+			for (int i = 0; i < newArray.Length; i++) {
+				newArray[i] = this.coefficients[i].Clone();
+			}
+
+			return new Term(newArray);
+		}
+
+		public double GetDoubleValue(ExpressionEvaluationArgs args) {
+			throw new NotImplementedException();
+		}
+
+		public ISolvable GetExactValue(ExpressionEvaluationArgs args) {
+			throw new NotImplementedException();
+		}
 	}
 }
