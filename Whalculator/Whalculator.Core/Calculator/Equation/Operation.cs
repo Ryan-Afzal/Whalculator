24b2c1242c8aa5afcdb430dce57b6b87f0266da4
Ryan-Afzal <ryan.afzal@outlook.com>
2019-09-30 18:02:47 -0700

    Worked on operation safety

--- Whalculator/Whalculator.Core/Calculator/Equation/Operation.cs
+++ Whalculator/Whalculator.Core/Calculator/Equation/Operation.cs
@@ -8,4 +8,18 @@ namespace Whalculator.Core.Calculator.Equation {
 
 	public delegate double DoubleValueOperation(ISolvable[] operands);
 
+	public struct Operation {
+
+		public static readonly Operation AddOperation = new Operation();
+
+		private Operation(ExactValueOperation exactValueOperation, DoubleValueOperation doubleValueOperation) {
+			ExactValueOperation = exactValueOperation;
+			DoubleValueOperation = doubleValueOperation;
+		}
+
+		public ExactValueOperation ExactValueOperation { get; private set; }
+		public DoubleValueOperation DoubleValueOperation { get; private set; }
+
+	}
+
 }
