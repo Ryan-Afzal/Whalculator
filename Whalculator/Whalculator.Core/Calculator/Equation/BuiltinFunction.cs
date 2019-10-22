38472ea780f3e2963d5ca726db8ec20ec5ac37f5
Ryan-Afzal <ryan.afzal@outlook.com>
2019-10-22 14:54:34 -0700

    Fixed builtinfunction to use NestedSolvable

--- Whalculator/Whalculator.Core/Calculator/Equation/BuiltinFunction.cs
+++ Whalculator/Whalculator.Core/Calculator/Equation/BuiltinFunction.cs
@@ -3,41 +3,27 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Whalculator.Core.Calculator.Equation {
-	public sealed class BuiltinFunction : ISolvable {
+	public sealed class BuiltinFunction : NestedSolvable {
 
 		private readonly BuiltinFunctionOperation operation;
 
-		private readonly ISolvable[] operands;
-
-		public BuiltinFunction(BuiltinFunctionOperation operation, params ISolvable[] operands) {
+		public BuiltinFunction(BuiltinFunctionOperation operation, params ISolvable[] operands) : base(operands) {
 			this.operation = operation;
 
 			if (operands.Length != this.operation.NumArgs) {
 				throw new InvalidEquationException(ErrorCode.InvalidNumArguments);
 			}
-
-			this.operands = operands;
 		}
 
-		private ISolvable[] CloneOperands() {
-			ISolvable[] output = new ISolvable[this.operands.Length];
-
-			for (int i = 0; i < output.Length; i++) {
-				output[i] = this.operands[i];
-			}
-
-			return output;
-		}
-
-		public ISolvable GetExactValue(ExpressionEvaluationArgs args) {
+		public override ISolvable GetExactValue(ExpressionEvaluationArgs args) {
 			return this.operation.ExactValueOperation.Invoke(this.operands, args);
 		}
 
-		public double GetDoubleValue(ExpressionEvaluationArgs args) {
+		public override double GetDoubleValue(ExpressionEvaluationArgs args) {
 			return this.operation.DoubleValueOperation.Invoke(this.operands, args);
 		}
 
-		public ISolvable Clone() {
+		public override ISolvable Clone() {
 			return new BuiltinFunction(
 				this.operation, 
 				this.CloneOperands());
