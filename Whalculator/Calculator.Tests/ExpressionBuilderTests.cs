84e2a398856aeb535fee974383089e2b9853e5c2
Ryan-Afzal <ryan.afzal@outlook.com>
2019-10-24 21:58:35 -0700

    Fixed the unit tests

--- Whalculator/Calculator.Tests/ExpressionBuilderTests.cs
+++ Whalculator/Calculator.Tests/ExpressionBuilderTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using Whalculator.Core.Calculator.Equation;
 
 namespace Whalculator.Tests {
@@ -6,15 +7,20 @@ namespace Whalculator.Tests {
 	public class ExpressionBuilderTests {
 
 		[TestMethod]
-		public static void TestBuildLiteral() {
+		public void TestBuildLiteral() {
 			var output = ExpressionBuilder.GetSolvableFromText("42.420");
-			Assert.Equals(42.420, output.GetDoubleValue(new ExpressionEvaluationArgs()));
+			Assert.AreEqual("42.42", output.GetEquationString());
 		}
 
 		[TestMethod]
-		public static void TestBuildOperatorBasic() {
+		public void TestBuildOperatorBasic() {
 			var output = ExpressionBuilder.GetSolvableFromText("1+1");
-			Assert.Equals(2, output.GetDoubleValue(new ExpressionEvaluationArgs()));
+			Assert.AreEqual("1+1", output.GetEquationString());
 		}
+
+		//[TestMethod]
+		//public void TestOOP() {
+		//	throw new NotImplementedException();
+		//}
 	}
 }
