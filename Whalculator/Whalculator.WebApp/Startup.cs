5ff697070a6260c5bfd4001450be1ba75079c831
Ryan-Afzal <ryan.afzal@outlook.com>
2020-01-11 16:37:54 -0800

    Update Startup.cs

--- Whalculator/Whalculator.WebApp/Startup.cs
+++ Whalculator/Whalculator.WebApp/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
