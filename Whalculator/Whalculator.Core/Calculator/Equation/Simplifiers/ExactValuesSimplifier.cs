d2b3a23d212111e3a64893c6cd0ac147e6378c33
Ryan-Afzal <ryan.afzal@outlook.com>
2020-02-25 14:56:59 -0800

    Tried to test new systems
    Found a bug in differentiation: The simplifier is having an infinite loop bug where it keeps shifting the differentiation operator around.

--- Whalculator/Whalculator.Core/Calculator/Equation/Simplifiers/ExactValuesSimplifier.cs
+++ Whalculator/Whalculator.Core/Calculator/Equation/Simplifiers/ExactValuesSimplifier.cs
@@ -8,7 +8,74 @@ namespace Whalculator.Core.Calculator.Equation.Simplifiers {
 		protected async override Task<(ISolvable, bool)> InvokeAsync(ISolvable solvable) {
 			if (solvable is NestedSolvable n) {
 				if (n is Operator o) {
-					throw new NotImplementedException();
+					if (o.Operation.Name == Operations.AddOperation.Name) {
+						int c = o.operands.Length;
+
+						/*
+						 * Add Literals
+						 */
+						for (int i = 0; i < o.operands.Length - 1; i++) {// Go from 0 to the penultimate operand
+							if (o.operands[i] is Literal l) {// If the loop never finds a literal, it's OK: if the last operand is a literal, it couldn't be simplified here anyway.
+								for (int k = i + 1; k < o.operands.Length; k++) {
+									if (o.operands[i] is Literal _l) {
+										o.operands[i] = new Literal(l.Value + _l.Value);
+										o.operands[k] = null!;
+										c--;
+									}
+								}
+
+								break;
+							}
+
+							if (!(o.operands[i] is Operator mult && mult.Operation.Name == Operations.MultiplyOperation.Name)) {
+								o.operands[i] = new Operator(Operations.MultiplyOperation, o.operands[i], new Literal(1));
+							}
+
+							var curr = (Operator)o.operands[i];
+
+							if (o.operands[i + 1] is Operator nextOperator) {
+								if (nextOperator.Operation.Name == Operations.ExponateOperation.Name && nextOperator.operands[0].Equals(curr.operands[0])) {
+									o.operands[i + 1] = new Operator(Operations.ExponateOperation, 
+										nextOperator.operands[0], 
+										new Operator(Operations.AddOperation, 
+											nextOperator.operands[1], 
+											curr.operands[1]
+											)
+										);
+									o.operands[i] = null!;
+									c--;
+								}
+							}
+						}
+
+						/*
+						 * Remove nulls and return
+						 */
+						ISolvable[] output = new ISolvable[c];
+						int _i = 0;
+						for (int i = 0; i < o.operands.Length; i++) {
+							if (o.operands[i] is object) {
+								output[_i] = o.operands[i];
+								_i++;
+							}
+						}
+
+						return (new Operator(Operations.AddOperation, output), true);
+					} else if (o.Operation.Name == Operations.MultiplyOperation.Name) {
+						throw new NotImplementedException();
+					} else if (o.Operation.Name == Operations.DivideOperation.Name) {
+						throw new NotImplementedException();
+					} else if (o.Operation.Name == Operations.ExponateOperation.Name) {
+						throw new NotImplementedException();
+					} else if (o.Operation.Name == Operations.DivideOperation.Name) {
+						throw new NotImplementedException();
+					} else {
+						for (int i = 0; i < o.operands.Length; i++) {
+							(o.operands[i], _) = await InvokeAsync(o.operands[i]);
+						}
+
+						return (o, true);
+					}
 				} else if (n is BuiltinFunction b) {
 					if (b.Operation.Name == BuiltinFunctionOperations.LnOperation.Name) {
 						throw new NotImplementedException();
--- Whalculator/Whalculator.SpeedTests/Program.cs
+++ Whalculator/Whalculator.SpeedTests/Program.cs
@@ -2,10 +2,11 @@
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Whalculator.Core.Calculator;
+using Whalculator.Core.Calculator.Equation.Simplifiers;
 
 namespace Whalculator.SpeedTests {
 	public class Program {
-		public static void Main(string[] args) {
+		public static async Task Main(string[] args) {
 			var calc = new Calculator();
 			var baseCalc = new BaseCalculator();
 			_ = baseCalc.GetResultValueAsync("1").Result;
@@ -20,13 +21,19 @@ namespace Whalculator.SpeedTests {
 			TestExpression(baseCalc, "ln(e)");
 			TestExpression(baseCalc, "ln(e + 100)");
 			TestExpression(baseCalc, "(2+(100^2*40+25)*100+10/2-354)^2");
-			Console.WriteLine("Ending Node Graph Tests.");
+			Console.WriteLine("Ended Node Graph Tests.");
 			
 			Console.WriteLine("Starting Input Tests...");
 			TestExpression(calc, "1");
 			TestExpression(calc, "1+1");
 			TestExpression(calc, "d/d -x x^2 + x*y + y^2");
-			Console.WriteLine("Ending Input Tests...");
+			Console.WriteLine("Ended Input Tests.");
+
+			Console.WriteLine("Starting Exact Value Tests...");
+			await TestExactValue(baseCalc, "1 + 5");
+			await TestExactValue(baseCalc, "1 + 5 + 3");
+			await TestExactValue(baseCalc, "1 + 5 + 3 + (-1)");
+			Console.WriteLine("Ended Exact Value Tests.");
 		}
 
 		private static void TestExpression(BaseCalculator calc, string exp) {
@@ -53,6 +60,19 @@ namespace Whalculator.SpeedTests {
 			Console.WriteLine($"\tResult: {result}");
 			Console.WriteLine($"\tElapsed Ticks: {timer.ElapsedTicks}");
 			Console.WriteLine($"\tElapsed Time: {timer.ElapsedMilliseconds}");
+		}
+
+		private static async Task TestExactValue(BaseCalculator calc, string exp) {
+			Console.WriteLine($"Input: {exp}");
+			var result = await (await calc.GetSolvableFromTextAsync(exp))
+				.GetSimplifier()
+				.AddLevelOperatorSimplifier()
+				.AddRationalExpressionsSimplifier()
+				.AddRemoveZerosOnesSimplifier()
+				.AddCollectLikeTermsSimplifier()
+				.AddSimplifier(new ExactValuesSimplifier())
+				.SimplifyAsync();
+			Console.WriteLine($"\tResult: {result}");
 		}
 	}
 }
